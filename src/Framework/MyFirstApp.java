e4d572cc98735659c1658845209ea2ebba0ad6c7
rahul <rahul.kumar_cs19@gla.ac.in>
2023-01-03 00:19:33 +0530

    Button Demo

---
+++ src/Framework/MyFirstApp.java
@@ -0,0 +1,31 @@
+package Framework;
+
+import java.awt.*;
+
+class MyFrame extends Frame
+{
+    Label l;
+    TextField tf;
+    Button b;
+
+    public MyFrame()
+    {
+        super("My App");
+        setLayout(new FlowLayout());
+
+        l = new Label("Name");
+        tf = new TextField(20);
+        b = new Button("Ok");
+
+        add(l);
+        add(tf);
+        add(b);
+    }
+}
+public class MyFirstApp extends Frame {
+    public static void main(String[] args) {
+        MyFrame f = new MyFrame();
+        f.setSize(400,400);
+        f.setVisible(true);
+    }
+}
